35049a637b6773735e7ad2046cc14d75fb527a90
Israel Dominic <iziedking@gmail.com>
2023-11-04 07:36:30 +0100

    New version

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 console.log('config.js');
 console.log('Version12');
 console.log('Updated')
-console.log("This is what it means to branch")
+console.log("This is what it means to branchg")
+console.log("Testing updated code")
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 console.log('index.js');
 console.log('version12');
 console.log("I am testing to know how to branch")
+console.log("Testing how to updated from another sorce code")
